2eb9002ce259b301a1ae7bd016488c9c139b30fa
Mylene Campana <mylene.campana@laas.fr>
2014-05-28 17:52:17 +0200

    Add cylinder_obstacle load lines

--- script/potential_test.py
+++ script/potential_test.py
@@ -16,7 +16,8 @@ robot.setJointBounds('base_joint_y',[-3, 3])
 
 r = ScenePublisher (robot)
 
-q1 = [-2.5, 1.0, 0.0] # x, y, theta
+# q = [x, y, theta]
+q1 = [-2.5, 1.0, 0.0]
 q2 = [2.5, 1.0, 0.0]
 #q2 = [1.8, 0.9, 1.57]
 
@@ -25,16 +26,16 @@ cl.problem.addGoalConfig (q2)
 p = PathPlayer (cl, r)
 
 # Load box obstacle in HPP for collision avoidanceq
-cl.obstacle.loadObstacleModel('potential_description','obstacle')
-cl.obstacle.moveObstacle ('obstacle_base', (0,0,0,1,0,0,0)) # to update load ?
+#cl.obstacle.loadObstacleModel('potential_description','obstacle')
+cl.obstacle.loadObstacleModel('potential_description','cylinder_obstacle')
+cl.obstacle.moveObstacle ('obstacle_base', (0,0,0,1,0,0,0)) # necessary ?
 
 cl.problem.solve ()
 
-# PB : saut au début de la solution (discontinuité dans theta)
 
 
 # Display centered box obstacle
-r.addObject('obstacle','obstacle_base') # display
+r.addObject('obstacle','obstacle_base')
 r(q1)
 
 
@@ -51,7 +52,7 @@ for n in  nodes:
 cl.obstacle.getObstaclePosition('obstacle_base')
 cl.robot.getJointOuterObjects('base_link')
 cl.robot.getCurrentConfig()
-cl.robot.setCurrentConfig(q4)
+cl.robot.setCurrentConfig(q1)
 cl.robot.collisionTest()
 res = cl.robot.distancesToCollision()
 cl.problem.pathLength(0)
