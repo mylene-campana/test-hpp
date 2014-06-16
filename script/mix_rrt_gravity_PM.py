df1304d100c4393fe69f1a8ed709a2d3e15e4e3c
Mylene Campana <mylene.campana@laas.fr>
2014-06-16 11:51:20 +0200

    Update to new DoF plot function

--- script/mix_rrt_gravity_PM.py
+++ script/mix_rrt_gravity_PM.py
@@ -5,11 +5,15 @@
 from hpp_ros import ScenePublisher
 from hpp_ros import PathPlayer
 from hpp.corbaserver.hrp2 import Robot
+#from hpp.corbaserver.humain import Robot
 from hpp.corbaserver import Client
+import sys
+sys.path.append('/local/mcampana/devel/hpp/src/test-hpp/script')
 
 Robot.urdfSuffix = '_capsule'
 Robot.srdfSuffix= '_capsule'
 robot = Robot ('hrp2_14')
+#robot = Robot ('humain')
 robot.setTranslationBounds (-2, 2, -2, 2, 0.605, 0.805) # rrt
 #robot.setTranslationBounds (-3, 10, -4, 4, -3, 5) # gravity
 cl = robot.client
@@ -17,6 +21,8 @@ r = ScenePublisher (robot)
 p = PathPlayer (cl, r)
 
 q1 = [0.0, 0.0, 0.705, 1.0, 0., 0., 0.0, 0.0, 0.0, 0.0, 0.0, -0.4, 0, -1.2, -1.0, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.261799, -0.17453, 0.0, -0.523599, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0]
+
+#q1 = [0.0, 0.0, 0.705, 1.0, 0., 0., 0.0, 0.0, 0.0, 0.0, 0.0, -0.4, 0, -1.2, -1.0, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.261799, -0.17453, 0.0, -0.523599, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0, 0, 0, 0, 0, 0]
 
 q2 = [0.0, 0.0, 0.705, 1, 0, 0, 0, 0.0, 0.0, 0.0, 0.0, 1.0, 0, -1.4, -1.0, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.261799, -0.17453, 0.0, -0.523599, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0]
 
@@ -46,71 +52,9 @@ cl.problem.clearRoadmap ()
 cl.problem.resetGoalConfigs ()
 
 
-# Ploting stuff ############################
-import matplotlib.pyplot as plt
-import numpy as np
-t_vec = np.arange(0., 8., 0.2)
-for t in t_vec:
-    #x = configAtSize(0, t)
-    plt.plot(cl.robot.configAtDistance(0, t)[0], \
-                 cl.robot.configAtDistance(0, t)[1], 'ro')
+# Plot interesting DoF #
+from trajectory_plot import dofPlot
+dofPlot(cl, 41, 42, 43 ,45)
+from trajectory_plot import plannarPlot
+# plannarPlot(cl) # must fix distancesToCollision() with min before call
 
-plt.axis([-2, 2, -2, 2]) #Xmin Xmax Ymin Ymax
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show() # works only once
-
-
-# Plot interesting DoF (here RLeg) ###
-import matplotlib.pyplot as plt
-import numpy as np
-dt = 0.1
-t_vec = np.arange(0., 8., dt) # all
-t_ssvec = np.arange(4.2, 6., dt)
-
-Tvec = t_vec [::]
-plt.subplot(221)
-for t in Tvec:
-    #plt.plot(t, cl.problem.configAtDistance(0, t)[41], 'ro')
-    plt.plot([t, t+dt], [cl.problem.configAtDistance(0, t)[41], \
-                             cl.problem.configAtDistance(0, t+dt)[41]], 'k-')
-
-plt.axis([min(Tvec),max(Tvec),-2,2]) #Xmin Xmax Ymin Ymax
-plt.xlabel('t')
-plt.ylabel('theta41')
-plt.grid()
-
-plt.subplot(222)
-for t in Tvec:
-    #plt.plot(t, cl.problem.configAtDistance(0, t)[42], 'ro')
-    plt.plot([t, t+dt], [cl.problem.configAtDistance(0, t)[42], \
-                             cl.problem.configAtDistance(0, t+dt)[42]], 'k-')
-
-plt.axis([min(Tvec),max(Tvec),-2,2]) #Xmin Xmax Ymin Ymax
-plt.xlabel('t')
-plt.ylabel('theta42')
-plt.grid()
-
-plt.subplot(223)
-for t in Tvec:
-    #plt.plot(t, cl.problem.configAtDistance(0, t)[43], 'ro')
-    plt.plot([t, t+dt], [cl.problem.configAtDistance(0, t)[43], \
-                             cl.problem.configAtDistance(0, t+dt)[43]], 'k-')
-
-plt.axis([min(Tvec),max(Tvec),-2,2]) #Xmin Xmax Ymin Ymax
-plt.xlabel('t')
-plt.ylabel('theta43')
-plt.grid()
-
-plt.subplot(224)
-for t in Tvec:
-    #plt.plot(t, cl.problem.configAtDistance(0, t)[45], 'ro')
-    plt.plot([t, t+dt], [cl.problem.configAtDistance(0, t)[45], \
-                             cl.problem.configAtDistance(0, t+dt)[45]], 'k-')
-
-plt.axis([min(Tvec),max(Tvec),-2,2]) #Xmin Xmax Ymin Ymax
-plt.xlabel('t')
-plt.ylabel('theta45')
-plt.grid()
-
-plt.show() # works only once
