43785d6392dcd0a15a651dc2ef73c91a2fb11c12
Mylene Campana <mylene.campana@laas.fr>
2014-11-26 13:08:37 +0100

    Update scripts to base_joint modifications

--- script/potential_test.py
+++ script/potential_test.py
@@ -2,7 +2,7 @@
 # Script which goes with potential_description package.
 # Load simple 'robot' cylinder and obstacle to test methods.
 # "roslaunch potential_description potential_cylinder_point.launch"
-# "roslaunch potential_description potential_obstacles.launch"
+# "potential_obstacles_concaves.launch"
 
 
 from hpp_ros import ScenePublisher
@@ -13,23 +13,19 @@ import time
 import sys
 sys.path.append('/local/mcampana/devel/hpp/src/test-hpp/script')
 
-
 robot = Robot ('simple_robot')
 cl = robot.client
-
 kRange = 5
-robot.setJointBounds('base_joint_x',[-kRange, kRange])
-robot.setJointBounds('base_joint_y',[-kRange, kRange])
-
+robot.setJointBounds('base_joint_xy', [-kRange, kRange, -kRange, kRange])
 r = ScenePublisher (robot)
 
-# q = [x, y, theta] #
-#q1 = [-2.5, 1.0, 0.0]; q2 = [2.5, 1.0, 0.0] # First
-#q1 = [-1.8, 0.3, 0.0]; q2 = [1.8, 0.3, 0.0] # First Point
-#q1 = [-2.5, 0.0, 0.0]; q2 = [2.5, 0.0, 0.0] # Second
-#q1 = [1.8, 0.9, 1.57]; q2 = [-1.3, -0.6, 1.57] # Third
-#q1 = [-4, 4, 0]; q2 = [4, -4, 0] # obstS 1
-q1 = [2.4, -4.6, 0]; q2 = [-0.4, 4.6, 0] # obstS 2
+# q = [x, y, z, theta] # (z not considered since planar)
+#q1 = [-2.5, 1.0, 1, 0.0]; q2 = [2.5, 1.0, 1, 0.0] # First
+#q1 = [-1.8, 0.3, 1, 0.0]; q2 = [1.8, 0.3, 1, 0.0] # First Point
+#q1 = [-2.5, 0.0, 1, 0.0]; q2 = [2.5, 0.0, 1, 0.0] # Second
+#q1 = [1.8, 0.9, 0.707, 0.707]; q2 = [-1.3, -0.6, 0.707, 0.707] # Third
+q1 = [-4, 4, 1, 0]; q2 = [4, -4, 1, 0] # obstS 1
+#q1 = [2.4, -4.6, 1.0, 0.0]; q2 = [-0.4, 4.6, 1.0, 0.0] # obstS 2
 
 cl.problem.setInitialConfig (q1)
 cl.problem.addGoalConfig (q2)
@@ -43,9 +39,15 @@ cl.problem.solve ()
 end=time.time()
 print "Solving time: "+str(end-begin)
 
-
 from trajectory_plot import planarConcObstaclesPlot # case multiple concaves obstacles
 planarConcObstaclesPlot(cl, 0, '', 0)
+
+num_log = 7770
+from parseLog import parseVisibilityNodes
+guardNodes = parseVisibilityNodes (num_log, 'INFO:/local/mcampana/devel/hpp/src/hpp-core/src/visibility-prm-planner.cc:176: q is a guard node: ')
+from trajectory_plot import planarConcObstaclesSpecNodesPlot
+planarConcObstaclesSpecNodesPlot(cl, 0, '', 0, guardNodes) # guards in blue
+
 
 len(cl.problem.nodes ())
 cl.problem.pathLength(0)
--- script/puzzle_test.py
+++ script/puzzle_test.py
@@ -1,5 +1,6 @@
 #/usr/bin/env python
 # Script which goes with puzzle_description package.
+# RViz command : roslaunch puzzle_description puzzle.launch
 # Easy way to test planning algo (no internal DoF) on SO3 joint.
 
 from hpp_ros import ScenePublisher
@@ -12,15 +13,13 @@ sys.path.append('/local/mcampana/devel/hpp/src/test-hpp/script')
 
 robot = Robot ('puzzle_robot')
 cl = robot.client
-
-robot.setJointBounds('base_joint_x',[-0.9, 0.9])
-robot.setJointBounds('base_joint_y',[-0.9, 0.9])
-robot.setJointBounds('base_joint_z',[-1.1, 1.1])
-
+robot.setJointBounds('base_joint_xyz', [-0.9, 0.9, -0.9, 0.9, -1.1, 1.1])
 r = ScenePublisher (robot)
 
-q1 = [0.0, 0.0, 0.6, 1.0, 0.0, 0.0, 0.0]
-q2 = [0.0, 0.0, -0.6, 1.0, 0.0, 0.0, 0.0] 
+q1 = [0.0, 0.0, 0.6, 1.0, 0.0, 0.0, 0.0]; q2 = [0.0, 0.0, -0.6, 1.0, 0.0, 0.0, 0.0]
+#q1 = [0.0, 0.0, 0.8, 1.0, 0.0, 0.0, 0.0]; q2 = [0.0, 0.0, -0.8, 1.0, 0.0, 0.0, 0.0]
+#q1 = [0.5, 0.3, 0.8, 0.7071067812, 0.0, 0.7071067812, 0.0]; q2 = [0.0, -0.6, -0.5, 1.0, 0.0, 0.0, 0.0]
+r(q1)
 
 cl.problem.setInitialConfig (q1)
 cl.problem.addGoalConfig (q2)
@@ -28,27 +27,20 @@ p = PathPlayer (cl, r)
 
 # Load box obstacle in HPP for collision avoidance
 cl.obstacle.loadObstacleModel('puzzle_description','decor_very_easy','')
+#cl.obstacle.loadObstacleModel('puzzle_description','decor_easy','')
+#cl.obstacle.loadObstacleModel('puzzle_description','decor','')
 
 begin=time.time()
 cl.problem.solve ()
 end=time.time()
 print "Solving time: "+str(end-begin)
 
-
 len(cl.problem.nodes ())
 cl.problem.pathLength(0)
-
-cl.problem.optimizePath (0)
 
 from trajectory_plot import xyzPlot
 pathNum = 0
 xyzPlot (cl, pathNum)
-
-#-----------------------------------------------------------------------#
-#q2 = [0.0, 0.0, -0.6, 0.707, 0.0, 0.0, 0.707]
-
-#cl.obstacle.loadObstacleModel('puzzle_description','decor','')
-#cl.obstacle.loadObstacleModel('puzzle_description','decor_easy','')
 
 # (Optional if environment ready) Display obstacle
 r.addObject('decor','decor_base') # display
@@ -72,26 +64,9 @@ cl.robot.getJointOuterObjects('j_object_three')
 cl.robot.setCurrentConfig(q1)
 cl.robot.collisionTest()
 cl.robot.distancesToCollision()
-from numpy import *
-argmin(cl.robot.distancesToCollision()[0])
 r( cl.problem.configAtDistance(0,5) )
 cl.problem.optimizePath (0)
 cl.problem.clearRoadmap ()
-
 from numpy import *
 argmin(cl.robot.distancesToCollision()[0])
 
-# Plot interesting DoF #
-from trajectory_plot import dofPlot
-num_path = 0 # 0 for non-optim path, 1 for optim path
-dofPlot(cl, num_path, 0, 1, 2 ,3)
-
-
-# Looking for nodes in fence (from roadmap)
-import time
-i=50
-while (i<100): # cl.problem.nodes ()[i][2] < -0. and
-    print i
-    r(cl.problem.nodes ()[i])
-    time.sleep (.4)
-    i=i+1
